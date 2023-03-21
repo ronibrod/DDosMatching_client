8a42a0156ffae0f1aea9ca7b7baf7ce3ba35d18e
roni <ronibrod@gmail.com>
2023-03-21 19:04:52 +0200

    Base with signIn and signUp

---
+++ src/views/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
+import '../styles/App.css'
+
+import SignInSide from './signIn/signIn'
+import SignUp from './SignUp/SignUp'
+
+function App() {
+  return SignUp();
+}
+
+export default App
